c4d646285e75cc2cac61eaa35cd43b4833136d67
Zoltan Biber <zoltan.biber@gmail.com>
2016-07-03 20:55:43 +0100

    Basic index page (work in progress) Sussing out what the model should do (unit test to be added) Basic quickhack validations for the params. Showing errors in the view. Main controller with index action.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,14 @@
 class MainController < ApplicationController
+	# make sure the user is logged in
+	# show user's favourites
+
   def index
+    params[:per_page] = '100' unless params[:per_page]
+    fizzbuzz = FizzBuzz.new(params)
+    if fizzbuzz.valid?
+      @numbers = fizzbuzz.numbers
+    else
+      redirect_to root_path, error: fizzbuzz.errors.full_messages
+    end
   end
 end
---
+++ app/models/fizz_buzz.rb
@@ -0,0 +1,60 @@
+class FizzBuzz
+	include ActiveModel::Validations
+
+	validate :biggest_number_allowed, :params_are_numbers, :params_are_allowed
+
+	attr_accessor :per_page, :offset
+
+	def initialize(params)
+		@per_page = params[:per_page]
+		@offset = params[:offset]
+	end
+
+	BIGGEST_ALLOWED_NUMBER = 100000000000
+	ALLOWED_PER_PAGE_VALUES = [50, 100, 200]
+	ALLOWED_OFFSET_VALUES = (1..(BIGGEST_ALLOWED_NUMBER / ALLOWED_PER_PAGE_VALUES[0]))
+
+	def biggest_number_allowed
+		if per_page.to_i * (offset ? offset.to_i + 1 : 0) > BIGGEST_ALLOWED_NUMBER
+			errors.add(:base, "The application cannot show numbers bigger than #{BIGGEST_ALLOWED_NUMBER}!")
+		end
+	end
+
+	def params_are_numbers
+		unless (per_page != '0' && per_page.to_i != 0) && (offset ? offset != '0' && offset.to_i != 0 : true)
+			errors.add(:base, 'Query parameters must be numbers.')
+		end
+	end
+
+	def params_are_allowed
+		unless ALLOWED_PER_PAGE_VALUES.include? per_page.to_i
+			errors.add(:base, "Per page parameters should be #{ALLOWED_PER_PAGE_VALUES.join(' or ')}." )
+		end
+	end
+
+	def numbers
+		if offset
+			range_start = (offset.to_i * per_page.to_i) + 1
+			range_end = range_start + per_page.to_i
+			values((range_start..range_end))
+		else
+			values((1..per_page.to_i))
+		end
+	end
+
+	private
+
+	def values(numbers)
+		numbers.map do |n|
+			if n % 3 == 0 && n % 5 == 0
+				'FizzBuzz'
+			elsif n % 3 == 0
+				'Fizz'
+			elsif n % 5 == 0
+				'Buzz'
+			else
+				n
+			end
+		end
+	end
+end
